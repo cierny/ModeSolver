6e0234abd8b6cafa4052019dce647b15affae1e8
ondrej <ondrej@mit.edu>
2020-08-18 17:45:00 -0400

    More robust integration

--- calc_coupling.py
+++ calc_coupling.py
@@ -14,17 +14,6 @@ w0 = 1.5e3
 mag = 20
 paa = 50e-6
 
-# cut = 1.25
-# NA = cut*2.405/(2*np.pi*r_core);
-# # NA = 0.11
-#
-# n_core = np.sqrt(n_clad**2 + NA**2)
-
-# def index(r):
-#     if r < r_core:
-#         return n_core
-#     else:
-#         return n_clad
 def index(r):
     if r < r_core:
         return n_clad + 0.0145*(1 - (r/r_core)**2)
@@ -56,20 +45,22 @@ if __name__ == '__main__':
     dr_max = f_col*mag*paa*1e6
     r_im, p_im = read_psf('fft_im.txt')
     r_re, p_re = read_psf('fft_re.txt')
+    psf_max = np.max(r_re)
     psf_im = InterpolatedUnivariateSpline(r_im, p_im, ext=1)
     psf_re = InterpolatedUnivariateSpline(r_re, p_re, ext=1)
 
+    tick = time.time()
     fit, mfd1, mfd2 = md.lp01(r_core, index, 2*np.pi/wl_tx)
     print('Tx w0 (s2x): %f' % (mfd1/2000))
     print('Tx w0 (fit): %f' % (mfd2/2000))
     print('f_col est: %f' % (w0*np.pi*mfd2/(2*wl_tx)*1e-3))
 
+    print('Solving modes...')
     md.initialize(r_core, index, 2*np.pi/wl_rx, True)
     modes = md.find_modes(bend=R)
     print('Rx modes: %d' % len(modes))
 
-    tick = time.time()
     rs = np.linspace(0, dr_max, 50)
-    res = md.coupling(psf_re, psf_im, modes, rs, np.inf)
+    res = md.coupling(psf_re, psf_im, psf_max, modes, rs, 10*r_core)
     print("--- %s seconds ---" % (time.time() - tick))
-    np.save('res_50_grin', res)
+    np.save('res_grin_3', np.vstack((rs,res)))
--- modes.py
+++ modes.py
@@ -67,16 +67,17 @@ def solve_mode(bz, b_min, b_max, l, init):
     r_core, index, k = fiber
     r0, y0, dy0 = init
     rz = np.arange(0, r0, r_core/npts)
+    yz = np.linspace(1 if l==0 else 0, y0, len(rz))
     rs = np.linspace(r0, r_core, npts)
     beta = least_squares(root_func, bz, bounds=(b_min, b_max), args=(l, init)).x[0]
     core = solve_ivp(field, [r0, r_core], [y0, dy0], args=(beta, l), t_eval=rs)
     r_full = np.concatenate((rz, rs[1:]))
-    y_full = np.concatenate((np.repeat(y0, len(rz)), core.y[0, 1:]))
+    y_full = np.concatenate((yz, core.y[0, 1:]))
     core_fit = splrep(r_full, y_full / np.max(y_full))
     w = np.sqrt(beta**2 - (k*index(r_core))**2)
     clad_fac = splev(r_core, core_fit) / kn(l, r_core*w)
     fit = lambda r: np.where(r < r_core, splev(r, core_fit), clad_fac*kn(l, r*w))
-    return beta, fit
+    return r0, beta, fit
 
 def find_modes(bend=1e6):
     if fiber == None:
@@ -95,12 +96,12 @@ def find_modes(bend=1e6):
         bzeros = find_bzeros(l, init, b_min, b_max, m_max)
         res = iterated_task(lambda b: solve_mode(b, b_min, b_max, l, init), bzeros)
         if len(res) > 0:
-            b_max = res[0][0]
+            b_max = res[0][1]
             m_max = len(res)
             for m, sol in enumerate(res):
-                beta, fit = sol
+                r0, beta, fit = sol
                 if ((beta*mult/k)**2-e1)/(e2-e1) > 0:
-                    modes.append(((l,m+1), beta, fit))
+                    modes.append(((l,m+1), r0, beta, fit))
             l = l + 1
         else:
             return modes
@@ -113,29 +114,33 @@ def lp01(r_core, index, k):
     initialize(r_core, index, k)
     b_max = k*index(0)
     b_min = k*index(r_core) + 1e-10
-    beta, fit = solve_mode(b_max, b_min, b_max, 0, find_init(0, b_min))
+    r0, beta, fit = solve_mode(b_max, b_min, b_max, 0, find_init(0, b_min))
     rs = np.linspace(0, 2*r_core, 100)
     sigma_fit = curve_fit(lambda r,s: np.exp(-r**2/(2*s**2)), rs, fit(rs)**2)[0][0]
     return fit, mfd(fit), 4*sigma_fit
 
-def coupling(psf_re, psf_im, modes, rs, r_max, tol=1e-1):
-    bounds = [0, 2*np.pi, 0, r_max]
+def coupling(psf_re, psf_im, psf_max, modes, rs, r_max, rtol=0, atol=1):
     results = np.zeros(len(rs))
     psf_int = lambda r: r*(psf_re(r)**2 + psf_im(r)**2)
-    psf_res = 2*np.pi*quad(psf_int, 0, r_max, epsabs=tol)[0]
+    psf_res = 2*np.pi*quad(psf_int, 0, psf_max, epsabs=atol, epsrel=rtol)[0]
     def fib_task(mode):
-        lm, beta, fit = mode
-        return dblquad(lambda r,t: r*(fit(r)*np.cos(lm[0]*t))**2, *bounds, epsabs=tol)[0]
+        lm, r0_mode, beta, fit = mode
+        bounds = [0, 2*np.pi, r0_mode, r_max]
+        return dblquad(lambda r,t: r*(fit(r)*np.cos(lm[0]*t))**2, *bounds, epsabs=atol, epsrel=rtol)[0]
+    print('Solving mode integrals...')
     fib_res = iterated_task(fib_task, modes)
-    def cross_task(fit, l, mode_res, r0):
+    def cross_task(fit, l, mode_res, r0_mode, r0):
+        bounds = [0, 2*np.pi, r0_mode, psf_max+r0]
         cross_int_re = lambda r,t: r*fit(r)*np.cos(l*t)*psf_re(np.sqrt(r**2+r0**2-2*r*r0*np.cos(t)))
         cross_int_im = lambda r,t: r*fit(r)*np.cos(l*t)*psf_im(np.sqrt(r**2+r0**2-2*r*r0*np.cos(t)))
-        cross_res_re = dblquad(cross_int_re, *bounds, epsabs=tol)[0]**2
-        cross_res_im = dblquad(cross_int_im, *bounds, epsabs=tol)[0]**2
+        cross_res_re = dblquad(cross_int_re, *bounds, epsabs=atol, epsrel=rtol)[0]**2
+        cross_res_im = dblquad(cross_int_im, *bounds, epsabs=atol, epsrel=rtol)[0]**2
         ni = (cross_res_re + cross_res_im)/(psf_res*mode_res)
         return ni if l==0 else ni/2
     for idx, mode in enumerate(modes):
-        lm, beta, fit = mode
-        c_task = lambda r: cross_task(fit, lm[0], fib_res[idx], r)
+        lm, r0_mode, beta, fit = mode
+        c_task = lambda r: cross_task(fit, lm[0], fib_res[idx], r0_mode, r)
+        print('\rSolving cross integrals for LP_%d,%d (%d/%d)' % (lm[0], lm[1], idx+1, len(modes)), end='')
         results = results + np.array(iterated_task(c_task, rs))
+    print('')
     return results
