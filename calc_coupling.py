23c61c58e31bd607a96f24bc32a6c672ad222f2b
ondrej <ondrej@mit.edu>
2020-08-18 19:00:59 -0400

    Fixed LP01 initialization

--- calc_coupling.py
+++ calc_coupling.py
@@ -4,7 +4,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.optimize import curve_fit
 import time
 
-r_core = 25
+r_core = 6.5
 n_clad = 1.45
 wl_tx = 0.78
 wl_rx = 0.98
@@ -13,6 +13,17 @@ R = 30e3
 w0 = 1.5e3
 mag = 20
 paa = 50e-6
+
+# cut = 0.73
+# NA = cut*2.405/(2*np.pi*r_core);
+#
+# n_core = np.sqrt(n_clad**2 + NA**2)
+#
+# def index(r):
+#     if r < r_core:
+#         return n_core
+#     else:
+#         return n_clad
 
 def index(r):
     if r < r_core:
@@ -41,13 +52,13 @@ def read_psf(filename):
     return np.array(r_samp), np.array(p_samp)
 
 if __name__ == '__main__':
-    f_col = 33.9e-3
-    dr_max = f_col*mag*paa*1e6
-    r_im, p_im = read_psf('fft_im.txt')
-    r_re, p_re = read_psf('fft_re.txt')
-    psf_max = np.max(r_re)
-    psf_im = InterpolatedUnivariateSpline(r_im, p_im, ext=1)
-    psf_re = InterpolatedUnivariateSpline(r_re, p_re, ext=1)
+    # f_col = 15.3944e-3
+    # dr_max = f_col*mag*paa*1e6
+    # r_im, p_im = read_psf('fft_im.txt')
+    # r_re, p_re = read_psf('fft_re.txt')
+    # psf_max = np.max(r_re)
+    # psf_im = InterpolatedUnivariateSpline(r_im, p_im, ext=1)
+    # psf_re = InterpolatedUnivariateSpline(r_re, p_re, ext=1)
 
     tick = time.time()
     fit, mfd1, mfd2 = md.lp01(r_core, index, 2*np.pi/wl_tx)
@@ -55,12 +66,12 @@ if __name__ == '__main__':
     print('Tx w0 (fit): %f' % (mfd2/2000))
     print('f_col est: %f' % (w0*np.pi*mfd2/(2*wl_tx)*1e-3))
 
-    print('Solving modes...')
-    md.initialize(r_core, index, 2*np.pi/wl_rx, True)
-    modes = md.find_modes(bend=R)
-    print('Rx modes: %d' % len(modes))
-
-    rs = np.linspace(0, dr_max, 50)
-    res = md.coupling(psf_re, psf_im, psf_max, modes, rs, 10*r_core)
-    print("--- %s seconds ---" % (time.time() - tick))
-    np.save('res_grin_3', np.vstack((rs,res)))
+    # print('Solving modes...')
+    # md.initialize(r_core, index, 2*np.pi/wl_rx, True)
+    # modes = md.find_modes(bend=R)
+    # print('Rx modes: %d' % len(modes))
+    #
+    # rs = np.linspace(0, dr_max, 50)
+    # res = md.coupling(psf_re, psf_im, psf_max, modes, rs, 10*r_core)
+    # print("--- %s seconds ---" % (time.time() - tick))
+    # np.save('res_smfgrin', np.vstack((rs,res)))
