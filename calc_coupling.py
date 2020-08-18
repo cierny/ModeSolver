3c27b781f9c94dbfad51bca5a5619ea63149283c
Ondrej Čierny <ondrej@mit.edu>
2020-08-18 14:15:57 -0400

    GRIN test

--- calc_coupling.py
+++ calc_coupling.py
@@ -4,7 +4,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.optimize import curve_fit
 import time
 
-r_core = 2.2
+r_core = 25
 n_clad = 1.45
 wl_tx = 0.78
 wl_rx = 0.98
@@ -14,15 +14,20 @@ w0 = 1.5e3
 mag = 20
 paa = 50e-6
 
-cut = 0.73
-NA = cut*2.405/(2*np.pi*r_core);
-# NA = 0.2
+# cut = 1.25
+# NA = cut*2.405/(2*np.pi*r_core);
+# # NA = 0.11
+#
+# n_core = np.sqrt(n_clad**2 + NA**2)
 
-n_core = np.sqrt(n_clad**2 + NA**2)
-
+# def index(r):
+#     if r < r_core:
+#         return n_core
+#     else:
+#         return n_clad
 def index(r):
     if r < r_core:
-        return n_core
+        return n_clad + 0.0145*(1 - (r/r_core)**2)
     else:
         return n_clad
 
@@ -47,24 +52,24 @@ def read_psf(filename):
     return np.array(r_samp), np.array(p_samp)
 
 if __name__ == '__main__':
-    f_col = 15.3919e-3
+    f_col = 33.9e-3
     dr_max = f_col*mag*paa*1e6
     r_im, p_im = read_psf('fft_im.txt')
     r_re, p_re = read_psf('fft_re.txt')
     psf_im = InterpolatedUnivariateSpline(r_im, p_im, ext=1)
     psf_re = InterpolatedUnivariateSpline(r_re, p_re, ext=1)
 
-    md.initialize(r_core, index, 2*np.pi/wl_rx, True)
-    modes = md.find_modes()
-    print('Modes: %d' % len(modes))
+    fit, mfd1, mfd2 = md.lp01(r_core, index, 2*np.pi/wl_tx)
+    print('Tx w0 (s2x): %f' % (mfd1/2000))
+    print('Tx w0 (fit): %f' % (mfd2/2000))
+    print('f_col est: %f' % (w0*np.pi*mfd2/(2*wl_tx)*1e-3))
 
-    fit, mfd1, mfd2 = md.lp01(r_core, index, 2*np.pi/wl_rx)
-    print('MFD (s2x): %f' % mfd1)
-    print('MFD (fit): %f' % mfd2)
+    md.initialize(r_core, index, 2*np.pi/wl_rx, True)
+    modes = md.find_modes(bend=R)
+    print('Rx modes: %d' % len(modes))
 
     tick = time.time()
     rs = np.linspace(0, dr_max, 50)
     res = md.coupling(psf_re, psf_im, modes, rs, np.inf)
-    # print('Coupling: %f' % res[0])
     print("--- %s seconds ---" % (time.time() - tick))
-    np.save('res', res)
+    np.save('res_50_grin', res)
